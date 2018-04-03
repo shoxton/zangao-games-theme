4ade5a0d357599d80f5841e04b9a5a151633ca84
Andre <andre.webderking@gmail.com>
2018-04-03 16:40:30 -0300

    add search template

---
+++ search.php
@@ -0,0 +1,43 @@
+<?php get_header(); ?>
+
+    <div class="page-con container">
+        <div class="row">
+            <div class="col-lg-9">
+
+                
+                    <?php if (have_posts()) : ?>
+                    <h2 class="archive-posts-header">
+                        Resultados para <?php the_search_query(); ?>
+                    </h2>
+                    <div class="d-flex row">
+                        <?php while (have_posts()) : the_post(); ?>
+                        <div class="col-12 col-sm-6 col-lg-4 col-margin">
+                            <?php get_template_part('content'); ?>
+                        </div>
+
+                        <?php endwhile; ?>
+
+                        <?php else : ?> 
+                            <div class="col-12">
+                                <?php get_template_part('content', 'not-found'); ?>
+                            </div>
+
+                    <?php endif; ?>
+                    </div>
+                
+                <div class="pagination">
+                    <?php echo paginate_links(); ?>
+                </div>
+                
+            </div>
+            <div class="col-lg-3">
+                <?php dynamic_sidebar('asidewidget'); ?>
+            </div>
+        </div>
+    </div>
+
+<?php get_footer(); ?>
+
+
+
+
